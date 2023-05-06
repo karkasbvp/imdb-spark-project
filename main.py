e4d6089d3749c7f1dcc917534d41da9ae93b56d3
unknown <volodymyr.burdaev@khpi.edu.ua>
2023-05-06 14:06:14 +0300

    Show name bases

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('abc').getOrCreate()
+print('Session Started')
+print('Code Executed Successfully')
 
 def main():
-    spark_session = ...
+   df = spark.read.csv('F:/2023_Python/imdb-spark-project/imdb-data/name.basics.tsv.gz', header=True)
+ # df = spark.read.csv('F:/2023_Python/imdb-spark-project/imdb-data/title.akas.tsv.gz', header=True)
+   df.show()
 
-    novies_df = spark_session.csv(path)
-    novies_df.show()
+    # novies_df = spark_session.csv(path)
+    # novies_df.show()
+
 if __name__ == "__main__":
     main()
