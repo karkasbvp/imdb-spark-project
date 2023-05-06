2f616f927517cc87dfa5bc1bc67614d6cab37922
unknown <volodymyr.burdaev@khpi.edu.ua>
2023-05-06 18:11:47 +0300

    Task2

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName('abc').getOrCreate()
+from pyspark.sql.functions import sum
+spark = SparkSession.builder.appName('SparkByExamples').getOrCreate()
+
+#spark = SparkSession.builder.appName('abc').getOrCreate()
 print('Session Started')
 print('Code Executed Successfully')
 
 def main():
-   df = spark.read.csv('F:/2023_Python/imdb-spark-project/imdb-data/name.basics.tsv.gz', header=True)
+   # spark_session = SparkSession.builder.master("local").appName("IMDB analysis app").getOrCreate()
+
+ # df = spark_session.read.csv('F:/2023_Python/imdb-spark-project/imdb-data/name.basics.tsv.gz')
+    df = spark.read.option(sep=r'\t').csv('F:/2023_Python/imdb-spark-project/imdb-data/name.basics.tsv.gz', header=True)
+    df.show()
+
  # df = spark.read.csv('F:/2023_Python/imdb-spark-project/imdb-data/title.akas.tsv.gz', header=True)
-   df.show()
+ #  df.sql_ctx.show()
+ #  df.limit(3).show()
+#   df.first()
+#   df.printSchema()
 
     # novies_df = spark_session.csv(path)
     # novies_df.show()
---
+++ task2.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('Karkasbvp').getOrCreate()
+
+df = spark.read.option("delimiter", "\t").csv('F:/2023_Python/imdb-spark-project/imdb-data/name.basics.tsv.gz', header=True)
+
+df.show()
+#df.printSchema()
+#df.describe('birthYear').show()
+#df.select('birthYear').show(5)
+#df.filter(1800<df.birthYear & df.birthYear<1900).show(5)
+#df.select(df.primaryName, df.birthYear, df.birthYear.between(1800,1899)).show(5)
+df.select(df.primaryName, df.birthYear) \
+    .where(df.birthYear.between(1800,1899)) \
+    .show()
+
+#df[df['primaryName'].str.contains('F')].show()
